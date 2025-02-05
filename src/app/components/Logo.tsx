f3549433827eaeec753f44271f666e8ede476ab6
mosidrum <mosiokanga@gmail.com>
2025-02-05 11:41:04 +0100

    header mobile responsiveness

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -5,8 +5,12 @@ import logo from "../assets/logo.png";
 export const Logo = () => {
   return (
     <div className="flex items-center gap-2 hover:cursor-pointer">
-      <Image src={logo} alt="logo" className="w-[24px] h-[32px]" />
-      <div className="text-[43px] font-bold text-textPrimary font-manrope">
+      <Image
+        src={logo}
+        alt="logo"
+        className="w-[16px] h-[24px] md:w-[24px] md:h-[32px]"
+      />
+      <div className="text-[35px] md:text-[43px] font-bold text-textPrimary font-manrope">
         Boldo
       </div>
     </div>
